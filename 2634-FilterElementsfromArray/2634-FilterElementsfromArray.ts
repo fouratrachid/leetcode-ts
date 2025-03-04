9c3917407bbae7788bee14ddf5809a54a412c50f
foufou <76008059+fouratrachid@users.noreply.github.com>
2025-03-05 00:46:58 +0100

    File updated at 3/5/2025, 12:46:58 AM

--- 2634-FilterElementsfromArray/2634-FilterElementsfromArray.ts
+++ 2634-FilterElementsfromArray/2634-FilterElementsfromArray.ts
@@ -1,11 +1,16 @@
+// Last updated: 3/5/2025, 12:46:58 AM
 type Fn = (n: number, i: number) => any;
 
 function filter(arr: number[], fn: Fn): number[] {
-    let result: number[] = [];
-    for (let i = 0; i < arr.length; i++) { 
-        if (fn(arr[i], i)) { 
-            result.push(arr[i]);
-        }
-    }
-    return result; 
+     let currentIndex = 0;
+
+ for (let i = 0; i < arr.length; i++) {
+   if (fn(arr[i], i)) {
+     arr[currentIndex] = arr[i];
+     currentIndex++;
+   }
+ }
+
+ arr.length = currentIndex;
+ return arr
 }
